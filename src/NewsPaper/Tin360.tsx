d0c22d85129bf10588bcf863002d2084c84961e4
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2023-07-20 22:37:26 +0700

    Update Tin360

--- src/NewsPaper/Tin360.tsx
+++ src/NewsPaper/Tin360.tsx
@@ -10,7 +10,7 @@ export default function Tin360() {
         marginTop: "2rem",
       }}
     >
-      <div style={{display:"flex", justifyContent:"center"}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Typography variant="h4">Tin 360</Typography>
       </div>
       <Box
@@ -21,76 +21,68 @@ export default function Tin360() {
           height: "100%",
           justifyContent: "space-evenly",
           alignContent: "flex-end",
-          marginTop:"1rem",
-          padding: "1.5rem"
+          marginTop: "1rem",
+          padding: "1.5rem",
         }}
       >
         <div style={{ width: "20%" }}>
           <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
+          <h3
+            style={{
+              width: "70%",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "22px",
+            }}
+          >
             Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
             vả
           </h3>
         </div>
         <div style={{ width: "20%" }}>
           <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
+          <h3 style={{
+              width: "70%",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "22px",
+            }}>
             Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
             vả
           </h3>
         </div>
         <div style={{ width: "20%" }}>
           <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
+          <h3 style={{
+              width: "70%",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "22px",
+            }}>
             Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
             vả
           </h3>
         </div>
         <div style={{ width: "20%" }}>
           <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
+          <h3 style={{
+              width: "70%",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "22px",
+            }}>
             Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
             vả
           </h3>
         </div>{" "}
         <div style={{ width: "20%" }}>
           <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
-            Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
-            vả
-          </h3>
-        </div>
-        <div style={{ width: "20%" }}>
-          <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
-            Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
-            vả
-          </h3>
-        </div>
-        <div style={{ width: "20%" }}>
-          <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
-            Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
-            vả
-          </h3>
-        </div>
-        <div style={{ width: "20%" }}>
-          <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
-            Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
-            vả
-          </h3>
-        </div>
-        <div style={{ width: "20%" }}>
-          <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
-            Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
-            vả
-          </h3>
-        </div>
-        <div style={{ width: "20%" }}>
-          <img src="https://images2.thanhnien.vn/zoom/160_100/528068263637045248/2023/7/1/anh-1-66-1688205162772268649110-154-0-1404-2000-crop-16882080602971404288768.jpg"></img>
-          <h3 style={{ width: "80%" }}>
+          <h3 style={{
+              width: "70%",
+              fontSize: "17px",
+              fontWeight: "700",
+              lineHeight: "22px",
+            }}>
             Mưa to trắng trời ở TP.HCM, nhiều nơi ngập nước khiến người dân vất
             vả
           </h3>
